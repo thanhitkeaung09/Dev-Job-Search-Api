b784bd502144b77e72c493d04079b15ffe67a436
thanhitkeaung09 <thanhtikeaungupwork@gmail.com>
2023-08-15 23:34:14 +0630

    applicant detail added

--- app/Services/ApplicantService/ApplicantService.php
+++ app/Services/ApplicantService/ApplicantService.php
@@ -6,8 +6,11 @@ namespace App\Services\ApplicantService;
 
 use App\Http\Resources\ApplicantResource;
 use App\Http\Response\ApiSuccessResponse;
+use App\Models\CV;
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\PaginatedResourceResponse;
+use Illuminate\Support\Facades\DB;
 
 class ApplicantService
 {
@@ -16,5 +19,17 @@ class ApplicantService
         $users = User::has('jobs')->with('jobs')->withCount('jobs')->paginate(5);
         // return new ApiSuccessResponse(ApplicantResource::collection($users));
         return new PaginatedResourceResponse(ApplicantResource::collection($users));
+    }
+
+    public function applicant_detail($type)
+    {
+        $cvs = CV::with('job.company')->where("user_id",$type)->get();
+        return $cvs;
+        
+    }
+
+    public function applicant_user_detail()
+    {
+        return "detail";
     }
 }

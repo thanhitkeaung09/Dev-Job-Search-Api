130339cda25669f72599f63088154d68957dc61b
thanhitkeaung09 <thanhtikeaungupwork@gmail.com>
2023-08-20 22:46:35 +0630

    websocket added to change in env

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
+// import axios from 'axios';
 import './bootstrap';
+
+// alert("websocket")
+// import Echo from 'laravel-echo';
+let publicChannel = Echo.channel("noti");
+publicChannel.subscribed(()=>{
+    console.log("subscribed");
+}).listen(".server.noti.send", (event) => {
+        console.log(event);
+     });
+
+// let publicChannel = Echo.channel("noti");
+// publicChannel
+//     .subscribed(() => {
+//         console.log("publicChannel subscribed");
+//     })
+//     // .listen(".server.message.like", (event) => {
+//     //     console.log(event);
+//     // });
+
+// const send = document.getElementById("send");
+
+
+
+// submit event is ok
+send.addEventListener("click",()=>{
+    console.log("helo");
+    axios.get("/user/like/list",{
+        message : "hello"
+    })
+})
